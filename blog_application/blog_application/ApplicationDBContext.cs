4cf55a4df009cd50932b9ec4a52e2dfa6ed3bb42
dhananjay7781 <Patil@1891>
2024-04-26 19:27:28 +0530

    UPDATE: Added database connection

---
+++ blog_application/blog_application/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using blog_application.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace blog_application
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- blog_application/blog_application/Program.cs
+++ blog_application/blog_application/Program.cs
@@ -1,3 +1,6 @@
+using blog_application;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,34 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", false, true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", true, true)
+.Build();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(
+        configuration.GetConnectionString("SQLServerConnection"),
+        sqlServerOptionsAction: sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 10,
+                maxRetryDelay: TimeSpan.FromSeconds(30),
+                errorNumbersToAdd: null);
+            sqlOptions.CommandTimeout(300);
+        });
+});
+
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+//    {
+//        Title = "Your API",
+//        Version = "v1"
+//    });
+//});
 
 var app = builder.Build();
 
